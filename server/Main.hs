2a23008e3bbad6fc17cf833ec9b2eb280820c0b3
Adam Chalmers <adam.s.chalmers@gmail.com>
2018-03-04 20:38:41 -0600

    Server now supports logical truth

--- server/Main.hs
+++ server/Main.hs
@@ -23,28 +23,53 @@ instance Yesod App
 
 mkYesod "App" [parseRoutes|
 / HomeR GET
-/api/truthTable/#String TruthTableR GET
+/api/truthTable/#String TruthTableR   GET
+/api/lTruth/#String     LogicalTruthR GET
 |]
 
 getHomeR = defaultLayout [whamlet|<h1>LogicZoo|]
 
 data Response
-    = Table   { table :: [([String], Bool)] }
-    | Err    { err  :: String }
+    = Table
+        { sentence :: String
+        , table :: [([String], Bool)]
+        }
+    | LTruth
+        { sentence :: String
+        , lTruth :: Bool
+        }
+    | Err
+        { err :: String }
     deriving (Show, Eq)
 
 instance ToJSON Response where
-    toJSON (Table x) = object [ "table" .= x ]
-    toJSON (Err x) = object [ "err" .= x ]
+    toJSON (Table s t)  = object [ "sentence" .= s, "table" .= t ]
+    toJSON (Err x)    = object [ "err" .= x ]
+    toJSON (LTruth s lt) = object [ "sentence" .= s, "lTruth" .= lt ]
 
 getTruthTableR :: String -> Handler RepJson
 getTruthTableR sentence = case table of
     Left err -> sendStatusJSON status400
         (Err { err = show err})
     Right rows -> sendStatusJSON status200
-        (Table { table = L.map (first Format.fmtModelList) rows})
+        (Table
+            { table = L.map (first Format.fmtModelList) rows
+            , sentence = sentence
+            })
     where
         table = fmap (truthTable Classical) (parse sentence)
+
+getLogicalTruthR :: String -> Handler RepJson
+getLogicalTruthR sentence = case result of
+    Left err -> sendStatusJSON status400
+        (Err { err = show err})
+    Right bool -> sendStatusJSON status200
+        (LTruth
+            { lTruth = bool
+            , sentence = sentence
+            })
+    where
+        result = fmap (logicalTruth Classical) (parse sentence)
 
 main :: IO ()
 main = warp 3000 App
